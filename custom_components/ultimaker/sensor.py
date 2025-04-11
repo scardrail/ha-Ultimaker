d27359130dde41a55ee8c6e7f656fb921c6b9679
scardrail <palportscar@hotmail.fr>
2025-04-11 10:47:12 +0200

    Suppression de TEMP_CELSIUS car il est remplacé par UnitOfTemperature.CELSIUS Suppression de TIME_MINUTES car nous utilisons déjà UnitOfTime.SECONDS

--- custom_components/ultimaker/sensor.py
+++ custom_components/ultimaker/sensor.py
@@ -13,8 +13,6 @@ from homeassistant.components.sensor import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     PERCENTAGE,
-    TEMP_CELSIUS,
-    TIME_MINUTES,
     UnitOfTemperature,
     UnitOfTime,
 )
